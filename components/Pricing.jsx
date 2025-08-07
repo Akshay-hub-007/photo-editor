c1fc02f77d720d4dc1d76cab32e5e5fb5e24154c
Akshay-hub-007 <akshaykalangi9@gmail.com>
2025-08-07 11:30:51 +0530

    manual pricing card adding

--- components/Pricing.jsx
+++ components/Pricing.jsx
@@ -4,9 +4,20 @@ import React from 'react'
 
 function Pricing() {
   return (
-    <div>
-       <PricingTable></PricingTable>
-    </div>
+    <section className='py-20' id="pricing">
+        <div className='max-w-6xl mx-auto px-6'>
+          <div className='text-center mb-16'>
+            <h2 className='text-5xl font-bold bg-gradient-to-r from-blue-400 to purple-500 bg-clip-text text-transparent mb-6'>
+             Pricing 
+            </h2>
+            <p className='text-xl text-gray-300 max-w-3xl mx-auto'>
+
+
+            </p>
+          </div>
+
+        </div>
+    </section>
   )
 }
 
